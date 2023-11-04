c29c6d4d9a55d8b31e7dde2db15128a143421061
Roboza <alters_raindrop@mail.ru>
2023-11-04 19:58:09 +0300

    fix settings

--- settings.py
+++ settings.py
@@ -1,17 +1,13 @@
 from os import getcwd, getenv
 from os.path import join
-from dotenv import load_dotenv
 
+API_KEY = '898f7eab63db25a204def0d3708f8753'
+URL = 'https://api.openweathermap.org/data/2.5/weather'
 
-load_dotenv()
-
-
-API_KEY = getenv('API_KEY')
-URL = getenv('URL')
-NAME_LOG_FILE = getenv('NAME_LOG_FILE')
-USER_LANGUAGE = getenv('USER_LANGUAGE')
-USER_UNITS = getenv('USER_UNITS')
-USER_ENCODING = getenv('USER_ENCODING')
+NAME_LOG_FILE = 'history.csv'
+USER_LANGUAGE = 'ru'
+USER_UNITS = 'metric'
+USER_ENCODING = 'windows-1251'
 NAME_FOLDER_RESOURCE_PROJECT = 'src'
 NAME_FOLDER_LOG_FILES = 'Logs_files'
 CURRENT_DIR = getcwd()
